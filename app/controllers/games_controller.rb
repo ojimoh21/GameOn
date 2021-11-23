87c7a6cb203b054264b663644ed33663dd9b6cba
Gulce Sakallioglu <gulcesakallioglu@gmail.com>
2021-11-23 14:54:28 +0000

    games controller updated

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,51 @@
+class GamesController < ApplicationController
+  before_action :find_game, only: %i[show edit update destroy]
+
+  def index
+    @games = Game.all
+  end
+
+  def show
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.user = current_user
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      puts "Couldn't save"
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @game.update(game_params)
+      redirect_to game_path(@game)
+    else
+      puts "Couldn't update"
+      render :edit
+    end
+  end
+
+  def destroy
+    @game.destroy
+    redirect_to games_path
+  end
+
+  private
+
+  def find_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:title, :description, :suggested_numplayers, :duration)
+  end
+end
