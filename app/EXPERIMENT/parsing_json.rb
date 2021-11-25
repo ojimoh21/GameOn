92861059c307a714e7d7af4d1ef6ba460b899098
alexandra okoro <alex.nnenna.okoro@gmail.com>
2021-11-25 23:19:18 +0000

    ok

--- app/EXPERIMENT/parsing_json.rb
+++ app/EXPERIMENT/parsing_json.rb
@@ -1,14 +1,14 @@
-require 'json'
-require 'open-uri'
+# require 'json'
+# require 'open-uri'
 
-url = 'https://api.boardgameatlas.com/api/search?list_id=5yCPKRYJoF&client_id=OShMmavExz'
-games_list = URI.open(url).read
-games = JSON.parse(games_list)['games']
+# url = 'https://api.boardgameatlas.com/api/search?list_id=5yCPKRYJoF&client_id=OShMmavExz'
+# games_list = URI.open(url).read
+# games = JSON.parse(games_list)['games']
 
-# puts games
+# # puts games
 
-games.each do |game|
-  puts "#{game['name']}"
-  puts "#{game['description']}"
-  puts "#{game['min_players']} - #{game['max_players']}"
-end
+# games.each do |game|
+#   puts "#{game['name']}"
+#   puts "#{game['description']}"
+#   puts "#{game['min_players']} - #{game['max_players']}"
+# end
