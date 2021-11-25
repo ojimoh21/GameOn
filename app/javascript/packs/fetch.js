53145f9e4aa0cf4fe698c4214ab2f32c118ca4ba
alexandra okoro <alex.nnenna.okoro@gmail.com>
2021-11-25 23:49:10 +0000

    fetch file added back

--- app/javascript/packs/fetch.js
+++ app/javascript/packs/fetch.js
@@ -1,17 +1,17 @@
-// const games = document.querySelector("#games");
-// fetch("https://api.boardgameatlas.com/api/search?list_id=5yCPKRYJoF&client_id=OShMmavExz")
-// // fetch(`https://api.boardgameatlas.com/api/search?name=${}&client_id=OShMmavExz`)
-//   .then(response => response.json())
-//   .then((data) => {
-//     console.log(data)
-//     data.games.forEach((game) => {
-//       const gameTag= `<li class="list-inline-item">
-//           <img src="${game['thumb_url']}" alt="">
-//           <p>${game['name']}</p>
-//            ${game['description']}
-//         </li>`;
-//      games.insertAdjacentHTML("beforeend", gameTag);
-//    });
-//   });
+const games = document.querySelector("#games");
+fetch("https://api.boardgameatlas.com/api/search?list_id=5yCPKRYJoF&client_id=OShMmavExz")
+// fetch(`https://api.boardgameatlas.com/api/search?name=${}&client_id=OShMmavExz`)
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data)
+    data.games.forEach((game) => {
+      const gameTag= `<li class="list-inline-item">
+          <img src="${game['thumb_url']}" alt="">
+          <p>${game['name']}</p>
+           ${game['description']}
+        </li>`;
+     games.insertAdjacentHTML("beforeend", gameTag);
+   });
+  });
 
-//   console.log("I'm here!");
+  console.log("I'm here!");
