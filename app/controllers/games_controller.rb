011eb4a78f42fa7652a4d8ee219f70f85cd4e450
Gulce Sakallioglu <gulcesakallioglu@gmail.com>
2021-11-29 14:51:43 +0000

    images uploaded to game from search

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
   before_action :find_game, only: %i[show edit update destroy]
 
@@ -16,8 +18,8 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.new(game_params)
-    user_game_file = URI.open(:image_url)
-    user_game.photo.attach(io: user_game_file, filename: :image_url, content_type: 'image/jpg')
+    game_image = URI.open(params[:game][:image_url])
+    @game.photo.attach(io: game_image, filename: :image_url, content_type: 'image/jpg')
     @game.user = current_user
     if @game.save
       redirect_to game_path(@game)
@@ -50,6 +52,6 @@ class GamesController < ApplicationController
   end
 
   def game_params
-    params.require(:game).permit(:title, :description, :suggested_numplayers, :duration, :photo, :image_url)
+    params.require(:game).permit(:title, :description, :suggested_numplayers, :duration, :photo)
   end
 end
