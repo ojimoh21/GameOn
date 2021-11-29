bdafcb4b4fcf7f8d9a31ca6383fcea4bd5b954e7
Gulce Sakallioglu <gulcesakallioglu@gmail.com>
2021-11-29 16:27:03 +0000

    fixed controller being called twice

--- app/javascript/controllers/games_api_controller.js
+++ app/javascript/controllers/games_api_controller.js
@@ -5,24 +5,20 @@ export default class extends Controller {
 
   static targets = ["query"]
 
-  connect() {
+  initialize() {
     console.log("Controller is here");
     const game = document.querySelector(".hot-games-carousel");
-    console.log(game);
     console.log("this is the hot games list")
     fetch("https://api.boardgameatlas.com/api/search?list_id=5yCPKRYJoF&client_id=OShMmavExz")
     .then(response => response.json())
     .then((data) => {
-      console.log(data.games)
       data.games.forEach((g) => {
-        console.log(g['image_url']);
         const gameTag = `<div class="mt-4 game">
           <img src="${g['image_url']}" alt="" class="games-img">
           </div>`;
         game.insertAdjacentHTML("beforeend", gameTag);
       });
     });
-    // console.log(this.queryTarget.value);
   }
 
   // update() {
