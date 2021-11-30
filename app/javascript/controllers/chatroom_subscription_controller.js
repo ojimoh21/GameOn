fa7fc6b9c3b260033429351319e3ebf5a95d949f
muinat jimoh <ojimoh21@live.co.uk>
2021-11-30 12:18:37 +0000

    chat finished

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -6,9 +6,10 @@ export default class extends Controller {
    connect() {
     this.channel = consumer.subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.element.insertAdjacentHTML("beforeend", data) }
+      { received: data => this.element.insertAdjacentHTML("beforeend", data)}
     )
-    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`);
+    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+
   }
   disconnect () {
   console.log("Unsubscribed from the chatroom")
