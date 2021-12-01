823b89db80ae1dbb1d7abba215fd462fdaa1c078
Tomas Truyols <tomast25@hotmail.com>
2021-12-01 15:50:19 +0000

    Adding the tinder-page 60% done

--- app/models/party_game.rb
+++ app/models/party_game.rb
@@ -2,6 +2,6 @@ class PartyGame < ApplicationRecord
   belongs_to :game
   belongs_to :party_session
 
-  has_many :votes
+  has_many :votes, dependent: :delete_all
   has_many :guests, through: :votes
 end
